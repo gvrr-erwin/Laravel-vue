0d640a641225f6d2fd1269f1af28c13032e5de35
gvrr-erwin <132018083+gvrr-erwin@users.noreply.github.com>
2024-05-02 14:28:13 +0800

    Delete js directory

--- js/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-import bootstrap from 'bootstrap/dist/js/bootstrap';
-window.bootstrap = bootstrap;
-
-import axios from 'axios';
-window.axios = axios;
